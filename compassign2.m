1c1dad3f09b70b52069e8d94d13eb0aa8b264acd
holls05 <leholly2103@gmail.com>
2024-03-05 20:11:51 -0800

    debugging prepping. unadded semicolons and added plot() to see changes w vectors

--- compassign2.m
+++ compassign2.m
@@ -6,37 +6,24 @@ theta = 2*pi / 6; % Angle for hexagon
 R = [cos(theta), -sin(theta); sin(theta), cos(theta)];
 
 % Initialize the matrix to store vectors
-M = zeros(2, 6); 
-M(:, 1) = u;
+M = zeros(2, 6) 
+M(:, 1) = u
+plot(M)
 
 % Generate five new vectors by rotating the initial vector
 for i = 2:6
-    M(:, i) = R^(i-1) * u;
+    M(:, i) = R^(i-1) * u
+    plot(M)
 end
 
      
-origin = zeros(1,size(M,2)); 
+origin = zeros(1,size(M,2));
+figure;
+plot(origin)
+figure;
 plot([origin; M(1,:)],[origin; M(2,:)]);
 
 
 
 
 
-% 2: Rotate a new theta
-
-newtheta = 8*pi/6
-figure;
-new_R = [cos(newtheta), -sin(newtheta); sin(newtheta), cos(newtheta)];
-
-% Initialize the matrix to store vectors
-new_M = zeros(2, 6); 
-new_M(:, 1) = u;
-
-% Generate five new vectors by rotating the initial vector
-for i = 2:6
-    new_M(:, i) = R^(i-1) * u;
-end
-
-     
-origin= zeros(1,size(new_M,2)); 
-plot([origin; new_M(1,:)],[origin; new_M(2,:)]);
