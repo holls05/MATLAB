5fd00e3884d39f0dc67a022ca53592559486ea8f
holls05 <leholly2103@gmail.com>
2024-03-07 23:23:03 -0800

    ttesting out 3

--- testca.m
+++ testca.m
@@ -1,2 +1,21 @@
-a1 = lecture2(1, 5)
-a2 = lecture2(2, 10)
+theta1= pi/2
+theta2 = pi/4
+
+T1 = [cos(theta1), -sin(theta1); sin(theta1), cos(theta1)]
+
+T2 = [cos(theta2), -sin(theta2); sin(theta2), cos(theta2)]
+
+
+T1xT2= T1*T2
+
+T2xT1 = T2*T1
+
+
+%For case 1 (same) plot T1(T2) and T2(T1)
+
+
+
+
+
+%For case 2 (different) plot T1(T2) and T2(T1)
+%Your graphs should be the same in case 1 and different in case 2
